1764da235a94d037fbdbcea0a48c5113ba2216f7
SiAdcock <simonadcock2@gmail.com>
2017-01-22 10:43:37 +0000

    extract testimonials logic into testimonials-page

--- testimonials-page.php
+++ testimonials-page.php
@@ -25,14 +25,47 @@
 
                         <article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
 
-                            <section class="entry-content cf" itemprop="articleBody" id="testimonials-content">
-                            </section>
+                                <section class="entry-content cf" itemprop="articleBody" id="testimonials-content">
 
-                            <footer class="article-footer">
+                                    <?php
+                                        $pages = get_pages(array('child_of' => 25));
 
-                            </footer>
+                                        foreach($pages as $page) {
+                                            $image = wp_get_attachment_image_src( get_post_thumbnail_id( $page->ID ))[0];
+                                    ?>
 
-                            <?php comments_template(); ?>
+                                    <div class="testimonial cf">
+                                        <h3><?php _e($page->post_title) ?></h3>
+                                        <p><?php _e($page->post_content) ?></p>
+
+                                        <?php
+                                            if ($image) {
+                                        ?>
+                                                <div class="m-all t-2of3 d-2of3">
+                                                    <div class="m-all t-1of3 d-1of3 testimonial__img">
+                                                        <img src="<?php _e($image) ?>" title="<?php _e($page->post_title) ?>" alt="<?php _e($page->post_title) ?>">
+                                                    </div>
+                                                </div>
+                                        <?php
+                                            } else {
+                                        ?>
+                                                <div class="m-all t-3of3 d-3of3"></div>
+                                        <?php
+                                    }
+                                        ?>
+                                    </div>
+
+                                    <?php
+                                        }
+                                    ?>
+
+                                </section>
+
+                                <footer class="article-footer">
+
+                                </footer>
+
+                                <?php comments_template(); ?>
 
                         </article>
 
